4f296f1ddaca2ef32191263aa918c2a79e163b67
Cody Durham <codydd@gmail.com>
2021-05-13 17:46:15 -0600

    Complete beverage model and schema

---
+++ models/beverage.js
@@ -0,0 +1,22 @@
+// import the connection
+const mongoose = require('../db/connection')
+
+//import the schema
+const Schema = mongoose.Schema
+
+const beverageSchema = new Schema({
+        "beverage-name": String,
+        "brand": String,
+        "beverage-type": String,
+        "contains-sugar": true,
+        "carbonated": true,
+        "container": String,
+},{timestamps: true})// I wanted to add a timestamp here.. even though its not needed. 
+
+//create the model = collection + schema
+//                              collection↓      schema↓
+const Beverage = mongoose.model('Beverage', beverageSchema)
+
+
+// export the schema
+module.exports = Beverage
